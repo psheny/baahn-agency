ba51440961eae23e70eab3778a8ded89e67a58cd
psheny <petro.psheny@gmail.com>
2022-02-16 20:26:54 +0200

    Added styles, postcss, postcss.config.js

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,1 +1,2 @@
+import '../styles/styles.css'
 alert('Hello, it is a test once again and again')
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,9 @@
 const path = require('path')
+// const postCSSPlugins = [
+//   require('postcss-simple-vars'),
+//   require('postcss-nested'),
+//   require('autoprefixer')
+// ]
 
 module.exports = {
   entry: './app/assets/scripts/App.js',
@@ -7,5 +12,26 @@ module.exports = {
     path: path.resolve(__dirname, 'app')
   },
   mode: 'development',
-  watch: true
+  watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          'css-loader?url=false',
+          {
+            loader: 'postcss-loader'
+            /* Didn't work, removed to postcss.config.js
+            (https://programmerah.com/error-reporting-using-postcss-34678/)
+
+            options: {
+              plugins: postCSSPlugins
+            }
+            */
+          }
+        ]
+      }
+    ]
+  }
 }
